b293ef09f48ccfee6d52ef65e0d7bcf54089c66a
Matteo Ghia <61846068+Maestro-Zacht@users.noreply.github.com>
2023-06-05 09:34:13 +0200

    tmp removed structures

--- ravworks_exporter/views.py
+++ ravworks_exporter/views.py
@@ -1,8 +1,57 @@
-from django.shortcuts import render
+import json
 
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
+from allianceauth.services.hooks import get_extension_logger
+
+from .forms import ExportForm
+
+logger = get_extension_logger(__name__)
 
 
 @login_required
 def index(request):
-    return render(request, 'ravworks_exporter/index.html')
+    if request.method == 'POST':
+        form = ExportForm(request.user, request.POST, request.FILES)
+        if form.is_valid():
+            error = False
+            config = json.load(form.cleaned_data['config'])
+            ownership = (
+                request.user.character_ownerships
+                .select_related('character')
+                .get(character__character_id=form.cleaned_data['character'])
+            )
+
+            if form.cleaned_data['skills'] == 'memberaudit':
+                from .exporters.skills.memberaudit import import_skills, is_character_added
+
+                if not is_character_added(ownership.character):
+                    messages.error(request, f"Character {ownership.character.character_name} is not added to MemberAudit")
+                    error = True
+                else:
+                    config.update(import_skills(ownership.character))
+            elif form.cleaned_data['skills'] == 'corptools':
+                from .exporters.skills.corptools import import_skills, is_character_added
+
+                if not is_character_added(ownership.character):
+                    messages.error(request, f"Character {ownership.character.character_name} is not added to CorpTools")
+                    error = True
+                else:
+                    config.update(import_skills(ownership.character))
+
+            # if form.cleaned_data['structures']:
+            #     pass
+
+            if not error:
+                pass
+
+    else:
+        form = ExportForm(request.user)
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'ravworks_exporter/index.html', context=context)
